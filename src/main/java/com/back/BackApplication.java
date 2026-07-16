8a0b5a57eb2f92192d5b5386cd964b9d560ac627
developerapp0721@gmail.com <developerapp0721@gmail.com>
2026-07-16 14:51:31 +0900

    setting

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -1,9 +1,11 @@
-package com.p141581;
+package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackApplication {
 
     public static void main(String[] args) {
